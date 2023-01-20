fa1300707f5a7b722f1a74d8f9c98fd8d4b3ad8b
Pr1thv1Raj <prajchauhan123456@gmail.com>
2023-01-20 23:10:11 +0530

    Add random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function getComputerChoice() {
+    const num = Math.floor((Math.random()*3) + 1);
+    let computerChoice = null;
+    switch (num) {
+        case 1: 
+              computerChoice = "rock";
+            break;
+        case 2:
+               computerChoice = "paper";
+            break;
+        case 3:
+                computerChoice = "scissors";
+    }
+    console.log(computerChoice);
+}
+
+getComputerChoice();
